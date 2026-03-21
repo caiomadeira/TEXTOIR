d097682cfda04265e3fbbd9063a8c0b7ba36c530
Caio Madeira <caiomadeiracontato@outlook.com>
2026-03-20 21:34:17 -0300

    download models scibert

--- download_models.py
+++ download_models.py
@@ -1,28 +1,28 @@
-# from transformers import AutoTokenizer, AutoModel
-# import os
+from transformers import AutoTokenizer, AutoModel
+import os
 
-# output_dir = "models/scibert_scivocab_uncased"
+output_dir = "models/scibert_scivocab_uncased"
 
-# if not os.path.exists(output_dir):
-#     os.makedirs(output_dir)
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
-# model_name = "allenai/scibert_scivocab_uncased"
-
-# tokenizer = AutoTokenizer.from_pretrained(model_name)
-# model = AutoModel.from_pretrained(model_name)
-# tokenizer.save_pretrained(output_dir)
-# model.save_pretrained(output_dir)
-# print('show')
-
-from transformers import AutoModel, AutoTokenizer
-
-model_name = "BAAI/bge-base-en-v1.5"
-save_path = "/home/caiosouza/nlp/models/bge-base-en-v1.5"
+model_name = "allenai/scibert_scivocab_uncased"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModel.from_pretrained(model_name)
+tokenizer.save_pretrained(output_dir)
+model.save_pretrained(output_dir)
+print('show')
 
-tokenizer.save_pretrained(save_path)
-model.save_pretrained(save_path)
+# from transformers import AutoModel, AutoTokenizer
 
-print(f"Modelo BGE instalado em: {save_path}")
+# model_name = "BAAI/bge-base-en-v1.5"
+# save_path = "/home/caiosouza/nlp/models/bge-base-en-v1.5"
+
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+# model = AutoModel.from_pretrained(model_name)
+
+# tokenizer.save_pretrained(save_path)
+# model.save_pretrained(save_path)
+
+# print(f"Modelo BGE instalado em: {save_path}")
